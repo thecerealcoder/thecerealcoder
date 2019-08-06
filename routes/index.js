33185e758f793b3ac5bd6e5af027c39ed513495d
thecerealcoder <thecerealcoder@gmail.com>
2019-08-05 18:10:54 -0700

    Successfully implemented pagination and fixed search bar animation

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,33 @@ router = express.Router();
 
 //Get posts from DB
 router.get("/", (req, res) => {
+    var perPage = 6;
+    var pageQuery = parseInt(req.query.page);
+    var pageNumber = pageQuery ? pageQuery : 1;
     if (req.query.search) {
         const regex = new RegExp(escapeRegex(req.query.search), 'gi');
         var message = null;
-        Post.find({ name: regex }).sort({ date: "descending" }).exec((err, allPosts) => {
-            if (err) {
-                console.log(err);
-            } else {
-                message = "Search results for: \"" + req.query.search + "\"";
-                if(allPosts.length === 0) {
-                    res.render("posts/index", { posts: allPosts, message: message });
+        Post.find({ name: regex }).skip((perPage * pageNumber) - perPage).limit(perPage).sort({ date: "descending" }).exec((err, allPosts) => {
+            Post.count({ name: regex }).exec((err, count) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect("back");
                 } else {
-                    res.render("posts/index", { posts: allPosts, message: message});
+                    message = "Search results for: \"" + req.query.search + "\"";
+                    res.render("posts/index", { posts: allPosts, current: pageNumber, pages: Math.ceil(count / perPage), message: message, search: req.query.search });
                 }
-            }
+            });
         });
     } else {
-        Post.find({}).sort({ date: "descending" }).exec((err, allPosts) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("posts/index", { posts: allPosts, message: message });
-            }
+        Post.find({}).skip((perPage * pageNumber) - perPage).limit(perPage).sort({ date: "descending" }).exec((err, allPosts) => {
+            Post.count().exec((err, count) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect("back");
+                } else {
+                    res.render("posts/index", { posts: allPosts, current: pageNumber, pages: Math.ceil(count / perPage), message: message, search: false });
+                }
+            });
         });
     }
 });
